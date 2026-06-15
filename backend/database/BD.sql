e86b494b06c50ce50d9fb6b0148d4bbc0b5b7120
Edwin Portugal <pme2028989@est.univalle.edu>
2026-06-15 19:27:02 -0400

    Diseñando tabla Usuarios

---
+++ backend/database/BD.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Usuarios (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nombre VARCHAR(100) NOT NULL,
+    correo VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    rol VARCHAR(20) DEFAULT 'cliente'
+);
