80b2ac562289c5e2399be7c41e4125678c5bc151
Edwin Portugal <pme2028989@est.univalle.edu>
2026-06-15 19:31:29 -0400

    Creando modelo Product

---
+++ backend/models/Product.js
@@ -0,0 +1,9 @@
+class Product {
+    constructor(nombre, precio, stock) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+}
+
+module.exports = Product;
