1a3b159f91830dcc551977600260f409ca4952cf
Edwin Portugal <pme2028989@est.univalle.edu>
2026-06-15 19:39:45 -0400

    Implementando registro de usuarios

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,3 +1,10 @@
 class UserController {}
 
 module.exports = UserController;
+class UserController {
+    registerUser() {
+        console.log("Registrando usuario");
+    }
+}
+
+module.exports = UserController;
