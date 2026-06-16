2ceea3cf96463fdfb5ed47e8fdbc9eff6bd506ad
Edwin Portugal <pme2028989@est.univalle.edu>
2026-06-15 22:32:13 -0400

    Agregando endpoint de login

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+router.post('/register', (req,res)=>{
+    res.send('Registro de usuario');
+});
+
+router.post('/login', (req,res)=>{
+    res.send('Login de usuario');
+});
+
 module.exports = router;
